03f54e62035078f86b3ff861bab3f956507274ab
techjanitor <puntme@gmail.com>
2016-03-04 16:22:51 -0600

    add test

---
+++ trace_test.go
@@ -0,0 +1,33 @@
+package trace
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
+	"golang.org/x/net/trace"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func performRequest(r http.Handler, method, path string) *httptest.ResponseRecorder {
+	req, _ := http.NewRequest(method, path, nil)
+	w := httptest.NewRecorder()
+	r.ServeHTTP(w, req)
+	return w
+}
+
+func TestTraceController(t *testing.T) {
+
+	gin.SetMode(gin.ReleaseMode)
+
+	router := gin.New()
+
+	router.Use(Trace())
+
+	r.GET("/debug/requests", TraceController)
+
+	first := performRequest(router, "GET", "/debug/requests")
+
+	assert.Equal(t, first.Code, 200, "HTTP request code should match")
+
+}
